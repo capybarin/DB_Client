2e41f1e5574f3a066bcdfc46148e1984f3a7d0af
capybarin <vladibzd@gmail.com>
2019-11-29 16:24:12 +0200

    Add/Remove buttons are created

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void buttonAddClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonRemoveClicked(ActionEvent actionEvent) {
+    }
 }
